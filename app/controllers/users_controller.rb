6c5259ae6ac9455cd07e751c95afa23f480b1e94
Satheesh Kumar <mail@satheesh.dev>
2021-05-29 10:57:56 +0530

    adds users & jwt based auth system

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :authorize_request, except: :create
+  before_action :set_user, except: %i[create index]
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors },
+              status: :unprocessable_entity
+    end
+  end
+
+  # GET /users/{username}
+  def show
+    render json: @user, status: :ok
+  end
+
+  # PUT /users/{username}
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors },
+        status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def set_user
+    @user = User.find_by_username!(params[:_username])
+    rescue ActiveRecord::RecordNotFound
+      render json: { errors: 'User not found' }, status: :not_found
+  end
+
+  def user_params
+    params.permit(:name, :username, :email, :password, :password_confirmation)
+  end
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :password,
+            length: {minimum: 6},
+            if: -> { new_record? || !password.nil? }
+
+  def as_json(options={})
+    super(options.merge({ except: [:password_digest] }))
+  end
+                
+end
