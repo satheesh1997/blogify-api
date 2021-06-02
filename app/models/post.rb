6a529b28c8ed5859836450529bc45e428f1b70b2
Satheesh Kumar <mail@satheesh.dev>
2021-06-02 20:46:07 +0530

    adds default value to status in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   validates :image, format: { with: URI::regexp(%w[http https]) }
 
   belongs_to :user
+
+  before_save :default_values
+
+  enum post_status: [:draft, :published, :archieved]
+
+  def default_values
+    self.status ||= "draft"
+  end
 end
