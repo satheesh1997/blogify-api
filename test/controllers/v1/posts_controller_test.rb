d82d17a90147e47ac50e65ab289b3efffa81106b
Satheesh Kumar <mail@satheesh.dev>
2021-06-17 11:32:47 +0530

    aligns the test files

---
+++ test/controllers/v1/posts_controller_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+
+module V1
+  class PostsControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
