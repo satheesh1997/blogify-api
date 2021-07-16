9a1d5144fb91120f9ada2ab1a8c8116a46f97983
Satheesh Kumar <satheesh.101097@gmail.com>
2021-07-16 14:14:30 +0530

    adds endpoints to create & destroy categories from a post (#12)

--- app/models/post_category.rb
+++ app/models/post_category.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PostCategory < ApplicationRecord
+  validates :category, uniqueness: { scope: :post }
+
   belongs_to :post
   belongs_to :category
 end
---
+++ test/controllers/v1/post_categories_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class V1::PostCategoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
